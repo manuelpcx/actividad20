e1fb6b0f41e6890af040b08023264499da828a78
Jesus Medina Aguilera <jesus.medina05@gmail.com>
2018-05-25 21:20:07 -0400

    modificacion de ejercicio 2

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -1,5 +1,11 @@
 module Formula
-  def perimetro
+
+  def perimetro(lado1, lado2)
+    (lado1 + lado2)*2
+  end
+
+  def area(lado1, lado2)
+    (lado1 * lado2)/2
   end
 end
 class Rectangulo
@@ -7,9 +13,9 @@ class Rectangulo
     @base = base
     @altura = altura
   end
-
+  include Formula
   def lados
-    '4, 5'
+    'La base es de 6cm y la altura de 4cm'
   end
 end
 
@@ -17,8 +23,12 @@ class Cuadrado
   def initialize(lado)
     @lado = lado
   end
-
+  include Formula
   def lados
-    '4, 5'
+    'El lado mide 8cm'
   end
 end
+
+Rectangulo.new(6,4)
+Cuadrado.new(8)
+Rectangulo.new.lados
