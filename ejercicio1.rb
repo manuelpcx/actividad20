dadfc50649ea18a8a2757bdf4f00bb1ceeb010e3
Jesus Medina Aguilera <jesus.medina05@gmail.com>
2018-05-25 20:52:56 -0400

    Realizacion de ejercicios

---
+++ ejercicio1.rb
@@ -0,0 +1,22 @@
+class Alumno
+  def initialize(nombre, nota1, nota2, nota3, nota4)
+    @nombre = nombre
+    @nota1 = nota1
+    @nota2 = nota2
+    @nota3 = nota3
+    @nota4 = nota4
+  end
+
+  def self.read_files( filename = 'notas.txt')
+    alumnos = []
+    data = []
+    File.open(filename, 'r') { |file| data = file.readlines }
+    data.each do |alumno|
+      alumnos << Alumno.new(*alumno.split(', '))
+    end
+    print alumnos
+  end
+end
+
+f = Alumno.read_files
+puts f
---
+++ ejercicio2.rb
@@ -0,0 +1,24 @@
+module Formula
+  def perimetro
+  end
+end
+class Rectangulo
+  def initialize(base, altura)
+    @base = base
+    @altura = altura
+  end
+
+  def lados
+    '4, 5'
+  end
+end
+
+class Cuadrado
+  def initialize(lado)
+    @lado = lado
+  end
+
+  def lados
+    '4, 5'
+  end
+end
